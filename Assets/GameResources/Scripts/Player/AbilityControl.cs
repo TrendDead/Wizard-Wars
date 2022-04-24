b9d7fc046d190d9dc7196fa4f85b06abed4d9524
TrendDead <icykot5hi@gmail.com>
2022-04-24 21:52:44 +0700

    Добавление балистического пути полета

---
+++ Assets/GameResources/Scripts/Player/AbilityControl.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Временный контроллер способностей
+/// </summary>
+public class AbilityControl : MonoBehaviour
+{
+    [SerializeField] private Ability _mainAbillity;
+    [SerializeField] private Transform _plauerTrans;
+
+    private bool _canShoot;
+
+    private void Start()
+    {
+        _canShoot = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && _canShoot)
+        {
+            _canShoot = false;
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        _mainAbillity.Shoot(_plauerTrans);
+        StartCoroutine(RechargeTime());
+    }
+
+    private IEnumerator RechargeTime()
+    {
+        yield return new WaitForSeconds(_mainAbillity.RechargeTime);
+        _canShoot = true;
+    }
+}
---
+++ Assets/GameResources/Scripts/Spells/Spell.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Компонент летящей способности
+/// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
+public class Spell : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _ps;
+    [SerializeField] private List<Effect> _effects;
+    //[SerializeField] private ParticleSystem _psDeath;
+
+    private Rigidbody2D _rb2d;
+
+    [HideInInspector] public float LifeTimeSpell;
+    
+    private void OnEnable()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+        //_ps.Play();
+    }
+
+
+    private void Update()
+    {
+        LifeTimeSpell -= Time.deltaTime;
+        if (LifeTimeSpell <= 0)
+            Destroy(gameObject);
+    }
+    /// <summary>
+    /// Старт таймера жизни спела
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator StartSpellTimer()
+    {
+        yield return new WaitForSeconds(LifeTimeSpell);
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        UseSpell();
+    }
+
+    private void UseEffect()
+    {
+        foreach (var effect in _effects)
+        {
+            effect.Abillity();
+        }
+    }
+
+    private void UseSpell()
+    {
+        //_ps.Stop();
+        //UseEffect();
+        Destroy(gameObject);
+    }
+
+}
